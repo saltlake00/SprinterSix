d9096d253201165afda1132c82b5f21851438a8d
saltlake00 <saltlake5470@gmail.com>
2026-05-04 11:42:31 +0900

    feat: 향상된 입력 매핑 설정

--- Source/SprinterSix/Actor/Character/SSPlayerController.cpp
+++ Source/SprinterSix/Actor/Character/SSPlayerController.cpp
@@ -2,3 +2,76 @@
 
 
 #include "SSPlayerController.h"
+
+void ASSPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASSPlayerController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	
+	//TODO: ULocalPlayer 알아보기
+	
+	//향상된입력 로컬플레이어 서브시스템을 가져온다. 
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	
+	//서브시스템으로 매핑컨텍스트를 추가해줌
+	Subsystem->AddMappingContext(ThirdPersonContext, 0);
+}
+
+void ASSPlayerController::OnUnPossess()
+{
+	Super::OnUnPossess();
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	
+	Subsystem->RemoveMappingContext(ThirdPersonContext);
+}
+
+void ASSPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComp = Cast<UEnhancedInputComponent>(InputComponent);
+	if (EnhancedInputComp == nullptr)
+	{
+		return;
+	}
+	
+	EnhancedInputComp->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::OnInputMoveTriggered);
+	EnhancedInputComp->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::OnInputLookTriggered);
+	EnhancedInputComp->BindAction(JumpAction, ETriggerEvent::Started,this, &ThisClass::OnInputJumpStarted);
+	EnhancedInputComp->BindAction(SprintAction, ETriggerEvent::Started, this, &ThisClass::OnInputSprintStarted);
+	EnhancedInputComp->BindAction(InteractionAction, ETriggerEvent::Started, this, &ThisClass::OnInputInteractionStarted);
+	
+}
+
+void ASSPlayerController::OnInputMoveTriggered(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp,Warning,TEXT("Input Move"))
+}
+
+void ASSPlayerController::OnInputLookTriggered()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Input Look"))
+	
+}
+
+void ASSPlayerController::OnInputJumpStarted()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Input Jump"))
+	
+}
+
+void ASSPlayerController::OnInputSprintStarted()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Input Sprint"))
+}
+
+
+void ASSPlayerController::OnInputInteractionStarted()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Input Interaction"))
+}
--- Source/SprinterSix/Actor/Character/SSPlayerController.h
+++ Source/SprinterSix/Actor/Character/SSPlayerController.h
@@ -4,8 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+
 #include "SSPlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+class UInputComponent;
 /**
  * 
  */
@@ -13,4 +20,39 @@ UCLASS()
 class SPRINTERSIX_API ASSPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void OnUnPossess() override;
+	
+protected:
+	virtual void SetupInputComponent() override;
+	
+	void OnInputMoveTriggered(const FInputActionValue& Value);
+	void OnInputLookTriggered();
+	void OnInputJumpStarted();
+	void OnInputSprintStarted();
+	void OnInputInteractionStarted();
+	
+	// input mapping	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputMappingContext> ThirdPersonContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> SprintAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> InteractionAction;
 };
