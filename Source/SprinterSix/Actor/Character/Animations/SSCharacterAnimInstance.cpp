1a8c5257b08ef5b48acdbafda2b03c59eb838d8e
saltlake00 <saltlake5470@gmail.com>
2026-05-08 21:01:55 +0900

    feat: 캐릭터 애니메이션 블루프린트 c++로 리팩토링

---
+++ Source/SprinterSix/Actor/Character/Animations/SSCharacterAnimInstance.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SSCharacterAnimInstance.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void USSCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	PlayerCharacter = Cast<ACharacter>(GetOwningActor());
+
+	if (PlayerCharacter == nullptr)
+	{
+		return;
+	}
+	
+	CharacterMovement = PlayerCharacter->GetCharacterMovement();
+}
+
+void USSCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (PlayerCharacter == nullptr)
+	{
+		return;
+	}
+
+	CharacterVelocity = CharacterMovement->Velocity;
+	GroundSpeed = CharacterVelocity.Size2D();
+
+	FRotator CharacterRotation = PlayerCharacter->GetActorRotation();
+	float RotationValue = CalculateDirection(CharacterVelocity, CharacterRotation);
+
+	if (CharacterMovement->bOrientRotationToMovement)
+	{
+		Direction = FMath::Clamp(RotationValue, -45, 45);
+	}
+	else
+	{
+		Direction = RotationValue;
+	}
+
+	bool bMoving = FMath::IsNearlyZero(GroundSpeed) == false;
+	bool bAccelerating = CharacterMovement->GetCurrentAcceleration().IsNearlyZero() == false;
+
+	if (bMoving && bAccelerating)
+	{
+		bShouldMove = true;
+	}
+
+	bIsFalling = CharacterMovement->IsFalling();
+}
---
+++ Source/SprinterSix/Actor/Character/Animations/SSCharacterAnimInstance.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SSCharacterAnimInstance.generated.h"
+
+class ACharacter;
+class UCharacterMovementComponent;
+/**
+ * 
+ */
+UCLASS()
+class SPRINTERSIX_API USSCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	//TODO: 스레드 세이프업데이트
+	//virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY()
+	TObjectPtr<ACharacter> PlayerCharacter;
+
+	UPROPERTY()
+	TObjectPtr<UCharacterMovementComponent> CharacterMovement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector CharacterVelocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float GroundSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Direction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bShouldMove;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsFalling;
+};
